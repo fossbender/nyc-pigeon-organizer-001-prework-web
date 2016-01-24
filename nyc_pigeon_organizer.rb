616174d462fb8d367a5b51ea3894414ade023077
fossbender <fossbender@gmail.com>
2016-01-24 17:40:08 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,27 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeons = []
+
+  data.each do | key, value |
+    pigeons << value.values
+  end
+ pigeon_hash = Hash[pigeons.flatten.collect { |name| [name, {}] } ]
+
+    data.each do |key, value|
+      value.each do | option, name_arr |
+        pigeon_hash.each do |name, specs|
+          if name_arr.include?(name)
+            if pigeon_hash[name][key].nil?
+              pigeon_hash[name][key] = [option.to_s]
+            else
+              pigeon_hash[name][key] << option.to_s
+            end
+        end
+      end
+      end
+    end
+
+  pigeon_hash
+
 end
